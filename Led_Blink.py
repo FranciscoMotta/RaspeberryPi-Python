bda928dd78302a41d2b91abc0dd57c1d968293a3
Francisco Motta <jmottaclaudio@gmail.com>
2021-02-24 15:18:48 -0500

    añadimos muestreo de estado

--- Led_Blink.py
+++ Led_Blink.py
@@ -6,6 +6,9 @@ import time
 ledPinBlink = 23
 buttonPinPullUp = 17
 
+primeraVariable = True
+segundaVariable = True
+
 #########DECLARACION DE ENTRADAS Y SALIDAS ##########
 
 GPIO.setmode(GPIO.BCM) # Nos referimos a los pines por su numero GPIO.BOARD tiene la misma funcion
@@ -25,11 +28,17 @@ while 1:
         time.sleep(0.1)
         GPIO.output(ledPinBlink, GPIO.LOW)
         time.sleep(0.1)
+        while primeraVariable:
+            print ("boton pulsado")
+            primeraVariable = False
+            segundaVariable = True
         
     else: #Boton pulsado
         
         GPIO.output(ledPinBlink, GPIO.LOW)
-        
-        
+        while segundaVariable:
+            print ("boton no pulsado")
+            segundaVariable = False
+            primeraVariable = True
         
         
